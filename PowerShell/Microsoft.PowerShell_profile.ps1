1daabed269a383d85f2a6b2a8d5e7713e76bf929
c-nao27 <78206853+c-nao27@users.noreply.github.com>
2025-03-14 22:36:21 +0900

    Update Microsoft.PowerShell_profile.ps1

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-function prompt {
+function Prompt {
     $wsl_home = "//wsl$/{distribution}/home/{username}"
     $pwd_path = $pwd.ProviderPath.Replace("\", "/")
     $wd = $pwd.ProviderPath.Equals($home) ? "~"
@@ -7,19 +7,11 @@ function prompt {
     $is_admin = [System.Security.Principal.WindowsPrincipal]::new([System.Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([System.Security.Principal.WindowsBuiltInRole]::Administrator)
     Write-Host "$env:username " -ForegroundColor "Green" -NoNewLine
     Write-Host $wd -ForegroundColor "Blue" -NoNewLine
-    if ($wd -gt 30) {
-        Write-Host ""
-    }
     return $is_admin ? "# " : "$ "
 }
 
-# tab補完をComplete関数へ割当（bashのcompleteに似た動作）
+# tab補完をcomplete関数(bashのcompleteに似た動作)へ割り当て
 Set-PSReadLineKeyHandler -Key TAB -Function Complete
 
-# import module
-# Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1
-# Install-Module DockerCompletion -Scope CurrentUser
-# Import-Module DockerCompletion
-
-# autoload
-ls $home\Documents\PowerShell\autoload\*.ps1 | % {.$_}
+# Autoload
+Join-Path ([System.IO.Path]::GetDirectoryName($profile)) Autoload | ls -Filter *.ps1 | % {.$_.FullName}
